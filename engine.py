257b5710ed796d8c066b056647ab77c2ffb6090e
NicholasOssine <ossinenicholas@gmail.com>
2026-07-31 21:51:31 -0400

    Add first attacker rule

--- engine.py
+++ engine.py
@@ -13,3 +13,18 @@ class Durak:
         self.talon = deck[12:]
         self.trump_card = self.talon[0]
         self.trump = suit(self.trump_card)
+        self.attacker = self.first_attacker()
+
+    def first_attacker(self):
+        best_player = 0
+        best_trump = None
+
+        for player in (0, 1):
+            hand = self.hands[player]
+            for card in hand:
+                if suit(card) == self.trump and (best_trump is None or card < best_trump):
+                    best_trump = card
+                    best_player = player
+
+        return best_player
+
