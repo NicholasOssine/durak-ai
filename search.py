b84e8c57fc4f9ec9a9ce3c9802006fb38febf65b
NicholasOssine <ossinenicholas@gmail.com>
2026-08-08 14:10:49 -0400

    Sample hidden cards into concrete game state

--- search.py
+++ search.py
@@ -7,6 +7,7 @@ def determinize(game, player):
     hidden = game.hidden_from(player)
     random.shuffle(hidden)
 
-    opponenet_hand_size = len(game.hands[opponent])
-    game.hands[opponent] = hidden[:opponenet_hand_size]
-    game.talon = hidden[opponenet_hand_size:]
+    opponent_hand_size = len(game.hands[opponent])
+    game.hands[opponent] = hidden[:opponent_hand_size]
+    game.talon = hidden[opponent_hand_size:]
+    return game
