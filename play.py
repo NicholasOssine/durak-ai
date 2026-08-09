858e98a35fdc4b4a9de25ef43d4235135a441562
NicholasOssine <ossinenicholas@gmail.com>
2026-08-09 14:45:48 -0400

    Alternate seats between games to account for small bias given first move logic

--- play.py
+++ play.py
@@ -12,11 +12,14 @@ def match(agent_a, agent_b, games=100):
     losses = [0, 0]
     draws = 0
 
-    for _ in range(games):
-        durak = play([agent_a, agent_b])
+    for game_number in range(games):
+        swapped = game_number % 2 == 1
+        agents = [agent_b, agent_a] if swapped else [agent_a, agent_b]
+
+        durak = play(agents)
         if durak is None:
             draws += 1
         else:
-            losses[durak] += 1
+            losses[1 - durak if swapped else durak] += 1
 
     print(f"A lost {losses[0] / games:.1%}, B lost {losses[1] / games:.1%}, draws {draws}")
