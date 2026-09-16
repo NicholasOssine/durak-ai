54453f5500ee32df768af5939a1cde32bd583ac2
NicholasOssine <ossinenicholas@gmail.com>
2026-09-15 21:12:04 -0400

    Search endgames with minimax

--- rust/src/endgame.rs
+++ rust/src/endgame.rs
@@ -1,9 +1,33 @@
-use crate::engine::Durak;
+use crate::engine::{Durak, Phase};
 
 fn utility(game: &Durak) -> i8 {
     match game.durak {
         None => 0,
         Some(1) => 1,
         Some(_) => -1,
+    }
+}
+
+fn minimax(game: &Durak) -> i8 {
+    if game.phase == Phase::Over {
+        return utility(game);
+    }
+
+    if game.current_player() == 0 {
+        let mut value = i8::MIN;
+        for action in game.get_actions() {
+            let mut child = game.clone();
+            child.apply(action);
+            value = value.max(minimax(&child));
+        }
+        value
+    } else {
+        let mut value = i8::MAX;
+        for action in game.get_actions() {
+            let mut child = game.clone();
+            child.apply(action);
+            value = value.min(minimax(&child));
+        }
+        value
     }
 }
